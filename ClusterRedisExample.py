f7c5be27a4f1cfbb9633cb6bde3afdb48cc0ac05
Bar Shaul <barshaul@amazon.com>
2021-08-24 19:19:22 +0300

    Added support for 'from_url' method

--- ClusterRedisExample.py
+++ ClusterRedisExample.py
@@ -4,6 +4,11 @@ from redis.cluster import ClusterNode
 host = 'localhost'
 startup_nodes = [ClusterNode(host, 6378), ClusterNode(host, 6379)]
 
+# from_url examples
+rc_url = Redis.from_url("redis://localhost:6379/0")
+print(rc_url.get('foo'))
+print(rc_url.keys())
+
 # rc = Redis(host=host, port=6379)
 rc = Redis(startup_nodes=startup_nodes)
 print(rc.get('foo'))
@@ -13,7 +18,7 @@ print(rc.cluster_save_config(rc.get_node(host=host, port=6378)))
 
 # READONLY examples
 rc_readonly = Redis(startup_nodes=startup_nodes, read_from_replicas=True)
-for i in range(0, 3):
+for i in range(0, 4):
     # Assigning the read command to the slot's servers in a Round-Robin manner
     print(rc_readonly.get('bar'))
 # set command would be directed only to the slot's primary node
@@ -22,3 +27,4 @@ rc_readonly.set('bar', 'foo2')
 print(rc_readonly.readwrite())
 # now the get command would be directed only to the slot's primary node
 print(rc_readonly.get('bar'))
+
